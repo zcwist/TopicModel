b88f68c85b6553b12dfe1a8954c655a6e366a4e8
zcwist <zcwist@gmail.com>
2017-09-19 11:25:45 -0700

    for Sara

--- HTMLProcessor.py
+++ HTMLProcessor.py
@@ -27,8 +27,16 @@ class ModuleTxt(object):
 
 		for entry in entries.find_all('li',class_="entry",recursive=False):
 			text = entry.find('div', class_="message user_content enhanced")
+			
 			if text != None:
-				response = {"id":entry.get('id'),"responseText":text.text.encode('utf-8'),"comments":[]}
+				student = ""
+				try:
+					studentname = entry.find('div',class_="discussion-header-content right-of-avatar").find('div',class_="pull-left span4").find('h2',class_="discussion-title").find('a',class_="author").text.strip()
+				except Exception as e:
+					print entry.find('div',class_="discussion-header-content right-of-avatar").find('div',class_="pull-left span4").find('h2',class_="discussion-title")
+					raise e
+				
+				response = {"id":entry.get('id'),"student":studentname,"responseText":text.text.encode('utf-8'),"comments":[]}
 
 				#comments
 				for li in entry.find('ul',class_='discussion-entries').find_all('li'):
@@ -47,25 +55,96 @@ class ModuleTxt(object):
 	def printComments(self,num_reponse, num_comment):
 		print self.responseList[num_reponse]["comments"][num_comment]["comment"]
 
+class ModuleTxtByList(ModuleTxt):
+	"""docstring for ModuleTxtByList"""
+	def __init__(self, topicFile):
+		#filter by good answer
+		self.namelist = set(["aaya abdelshafy","sarah alsamman","lal altuner","aramis anderson","catalina biggerstaff","alexander cassidy","courtnie chan","inaara charolia","elva chen","justin chen","angela colback","dion dong","marina fraile-romero","shorya ghai","manuel gonzalez","rachel hokanson","samantha holder","iris hou","destiny hwang","reed jones","samantha lamos","akemi levine","stephanie luna-lopez","kathleen masancay","peter phan","nicola phillips","mailess phiri","abhishek punia","zoe randolph","priyanka saiprasad","neil shankar","molly simon","kate simonds","alyssa so","jake trambert","matt whittle","richard wong","irene yu","peggy zhao"])
+
+		#filter by bad answer
+		self.namelist = set(["tait adams","anam ahsani","carlos allende bres","shreya chaganti","maggie chen","andrew huang","suzi hyun","sparsh jain","brytni johnston","lauren kirkpatrick","francesca ledesma","xuanzhao li","regina madanguit","alix mcmenamy","john ospina","elmer pangilinan","arsalan qureshi","lauren silver","melissa smith","arvin villadelgado","sharon wang","katherine whitman","lucy zhang","luke zhang"])
+		self.student20 =[]
+		super(ModuleTxtByList, self).__init__(topicFile)
+	
+	def responses(self):
+		responses = []
+		entries = self.soup.find(id="discussion_subentries").find('ul',class_="discussion-entries")
+		# print entries
+
+		for entry in entries.find_all('li',class_="entry",recursive=False):
+			# print entry
+			try:
+				studentname = entry.find('h2').find('a').string.strip()
+				# print studentname.lower() in self.namelist
+				
+				if studentname.lower() not in self.namelist:
+					continue
+				self.student20.append(studentname)
+
+			except Exception as e:
+				pass
+			
+
+			
+			text = entry.find('div', class_="message user_content enhanced")
+			if text != None:
+				response = {"id":entry.get('id'),"responseText":text.text.encode('utf-8'),"comments":[]}
+
+				#comments
+				for li in entry.find('ul',class_='discussion-entries').find_all('li'):
+					comment = li.find('div',class_='message user_content enhanced')
+					if comment != None:
+						# print comment.text.encode('utf-8')
+						# print "***"
+						response["comments"].append({"comment_id":li.get('id'),"comment":comment.text.encode('utf-8')})
+
+				responses.append(response)
+		return responses	
+
 
 if __name__ == '__main__':
-	topic1 = "Topic_ Module 1 Discussion_ Innovation Processes - Social Sector Solutions.htm"
-	# topic2 = "PFPS Module 1/Topic_ Module 1 Discussion_ Innovation Processes - IBD Team Members page 2.html"
-	# topic2 = "Topic_ Module 2 Discussion_ Redesigning Businesses - Social Sector Solutions.html"
-	module1 = ModuleTxt(topic1)
-	print module1.question()
-	# for i,response in enumerate(module1.responses()):
-	# 	print "response"+str(i)
-	# 	print response["responseText"]
-	# 	print "========================"
+	def topic1():
+		topic1 = "Topic_ Module 1 Discussion_ Innovation Processes - Social Sector Solutions.htm"
+		# topic2 = "PFPS Module 1/Topic_ Module 1 Discussion_ Innovation Processes - IBD Team Members page 2.html"
+		# topic2 = "Topic_ Module 2 Discussion_ Redesigning Businesses - Social Sector Solutions.html"
+
+		module1 = ModuleTxt(topic1)
+		print module1.question()
+		# for i,response in enumerate(module1.responses()):
+		# 	print "response"+str(i)
+		# 	print response["responseText"]
+		# 	print "========================"
 
 
 
 
-	
-	# print module1.responseList[0]["responseText"]["comments"][1]["comment"]
-	# print module1.responseList[14]["comments"][1]["comment"]
-	# module1.printResponse(1)
-	# module1.printComments(16,1)
+		
+		# print module1.responseList[0]["responseText"]["comments"][1]["comment"]
+		# print module1.responseList[14]["comments"][1]["comment"]
+		# module1.printResponse(1)
+		# module1.printComments(16,1)
+	def module2():
+		module2topic = "module 2/Collaborative Innovaiotn Topic_ Week 9 Discussion_ Framing and Reframing.html"
+		module2 = ModuleTxtByList(module2topic)
+		# module2 = ModuleTxt(module2topic)
+
+		module2_2topic = "module 2/Collaborative Innovation part 2 Topic_ Week 9 Discussion_ Framing and Reframing.html"
+		module2_2 = ModuleTxtByList(module2_2topic)
+
+		from collections import Counter
+		studentlist = module2.student20 + module2_2.student20
+		print Counter(studentlist)
+		print len(studentlist)
+		print len(module2.responses())
+		print len(module2_2.responses())
+	def fall2017():
+		module2topic = "Topic_ Week 1_ Reading Reflection.html"
+		week1 = ModuleTxt(module2topic)
+		for i,response in enumerate(week1.responses()):
+			print "response"+str(i+1)
+			print response["student"]
+			print response["responseText"]
+			print "========================"
+	fall2017()
 
 
--- NormalizedArticleAnalysis.py
+++ NormalizedArticleAnalysis.py
@@ -9,7 +9,7 @@ from gensim.parsing.preprocessing import STOPWORDS
 from os import listdir, mkdir
 from os.path import isfile, join 
 
-from HTMLProcessor import ModuleTxt
+from HTMLProcessor import ModuleTxt, ModuleTxtByList
 import Config
 
 from gensim.models.tfidfmodel import TfidfModel
@@ -95,29 +95,30 @@ class Corpus(object):
 			self.corpus_list.append(corpus)
 
 	def tfidf(self,docs):
-		dictionary = Dictionary(docs)
+		if len(docs) > 1:
+			dictionary = Dictionary(docs)
 
-		corpus = [dictionary.doc2bow(doc) for doc in docs]
-		# print len(corpus[0])
-		# print len(corpus[1])
-		# print len(corpus[2])
+			corpus = [dictionary.doc2bow(doc) for doc in docs]
+			# print len(corpus[0])
+			# print len(corpus[1])
+			# print len(corpus[2])
 
-		tfidf = TfidfModel(corpus)
-		# print tfidf
+			tfidf = TfidfModel(corpus)
+			# print tfidf
 
-		def topn(doc,n):
-			import heapq
-			topn = heapq.nlargest(n, (k for k in doc), key=lambda k: k[1])
-			new_doc = []
-			for t in topn:
-				new_doc.append(dictionary[t[0]])
-			return new_doc
+			def topn(doc,n):
+				import heapq
+				topn = heapq.nlargest(n, (k for k in doc), key=lambda k: k[1])
+				new_doc = []
+				for t in topn:
+					new_doc.append(dictionary[t[0]])
+				return new_doc
 
-		docs = []
+			docs = []
 
 
-		for doc in tfidf[corpus]:
-			docs.append(topn(doc,505))
+			for doc in tfidf[corpus]:
+				docs.append(topn(doc,505))
 
 		self.docs = docs
 
@@ -140,7 +141,7 @@ class NormArticleAnalysis(object):
 		super(NormArticleAnalysis, self).__init__()
 		self.updateModel = updateModel
 		self.topicNumber = topicNumber
-		self.moduleText = ModuleTxt(moduleFile)
+		self.moduleText = ModuleTxtByList(moduleFile)
 		self.topicTerm = {}
 		self.articleName = ""
 		self.lda()
@@ -158,13 +159,15 @@ class NormArticleAnalysis(object):
 				mkdir("ldaModel/module"+str(self.topicNumber) +  "/normalizedArticle/")
 			except Exception as e:
 				pass
-			corpus= Corpus('pre-processed_data/topic'+str(self.topicNumber) + '/')
+			corpus= Corpus('pre-processed_data/module'+str(self.topicNumber) + '/')
 			dictionary = corpus.dictionary
 			# print dictionary.values()
 			
 			self.ldaModel = LdaModel(corpus=corpus.corpus_list[0], num_topics=Config.TOPIC_NUMBER,id2word=dictionary,passes=100,iterations=400,alpha="auto",eta=0.01)
 			self.ldaModel.update(corpus.corpus_list[1])
 			self.ldaModel.update(corpus.corpus_list[2])
+			self.ldaModel.update(corpus.corpus_list[3])
+			self.ldaModel.update(corpus.corpus_list[4])
 			addtional_corpus = corpus.build_additional_corpus(self.moduleText.questionTxt)
 			self.ldaModel.update(addtional_corpus)
 			self.ldaModel.save(topicModelFile)
@@ -313,30 +316,57 @@ def predict_subquestion_answered(topicmodel, seg, questionNum):
 
 
 if __name__ == '__main__':
-	# corpus = Corpus('pre-processed_data/topic1/')
-	# print corpus.corpus_list
-	# print corpus.docs
-	# print corpus.paragraphs[0]
+	def module1():
+		# corpus = Corpus('pre-processed_data/topic1/')
+		# print corpus.corpus_list
+		# print corpus.docs
+		# print corpus.paragraphs[0]
 
 
-	module1 = "Topic_ Module 1 Discussion_ Innovation Processes - Social Sector Solutions.htm"
+		module1 = "Topic_ Module 1 Discussion_ Innovation Processes - Social Sector Solutions.htm"
 
-	articleAnalysis1 = NormArticleAnalysis(1,module1,updateModel=False)
-	articleAnalysis1.printTopicTerms()
-	# print articleAnalysis1.ldaModel.show_topics(num_topics=Config.TOPIC_NUMBER, num_words=5,formatted=True)
+		articleAnalysis1 = NormArticleAnalysis(1,module1,updateModel=False)
+		articleAnalysis1.printTopicTerms()
+		# print articleAnalysis1.ldaModel.show_topics(num_topics=Config.TOPIC_NUMBER, num_words=5,formatted=True)
 
-	questionVec = articleAnalysis1.questionVector()
-	print questionVec
-	print 
-
-
+		questionVec = articleAnalysis1.questionVector()
+		print questionVec
+		print 
 
 
-	for x in range(22):
-		vec =  articleAnalysis1.responseAnalysis(x)
-		# print vec 
-		print cos_sim(vec,questionVec)
 
-	# vec = articleAnalysis1.responseAnalysis(0)
-	# print vec
-	# print cos_sim(vec,questionVec)
+
+		for x in range(22):
+			vec =  articleAnalysis1.responseAnalysis(x)
+			# print vec 
+			print cos_sim(vec,questionVec)
+
+		# vec = articleAnalysis1.responseAnalysis(0)
+		# print vec
+		# print cos_sim(vec,questionVec)
+
+	def module2():
+		module2 = "module 2/Collaborative Innovaiotn Topic_ Week 9 Discussion_ Framing and Reframing.html"
+		articleAnalysis2 = NormArticleAnalysis(2, module2,updateModel=False)
+
+		articleAnalysis2.printTopicTerms()
+		questionVec = articleAnalysis2.questionVector()
+		print questionVec
+
+		for x in range(len(articleAnalysis2.moduleText.responses())):
+			vec = articleAnalysis2.responseAnalysis(x)
+			print cos_sim(vec,questionVec)
+
+		module2_2 = "module 2/Collaborative Innovation part 2 Topic_ Week 9 Discussion_ Framing and Reframing.html"
+		articleAnalysis2_2 = NormArticleAnalysis(2, module2_2,updateModel=False)
+		for x in range(len(articleAnalysis2_2.moduleText.responses())):
+			vec = articleAnalysis2_2.responseAnalysis(x)
+			print cos_sim(vec,questionVec)
+
+		# print len(articleAnalysis2.moduleText.responses())
+		# print len(articleAnalysis2_2.moduleText.responses())
+
+
+
+	module2()
+
